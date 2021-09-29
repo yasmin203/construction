1b46a6992c4c59c3c6d346d62ad2ec2ec6890b3e
yasmin203 <anayasoo2031995@gmail.com>
2021-09-29 21:35:50 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function expand(){
+    var x=document.getElementById("form1");
+    // if (x.className === "d-none") {
+    //     x.className = "d-block";
+    //   } else {
+    //     x.className  = "d-none";
+    //   }
+          x.style.paddingLeft="10px";
+          x.classList.toggle("d-inline-block");
+}
