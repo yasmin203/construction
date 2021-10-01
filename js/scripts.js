20d8f658b26b61f2582d432225797f1dccd653e5
yasmin203 <anayasoo2031995@gmail.com>
2021-10-01 20:12:52 +0300

    add effect on gallary ith js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
-function expand(){
-    var x=document.getElementById("form1");
-    // if (x.className === "d-none") {
-    //     x.className = "d-block";
-    //   } else {
-    //     x.className  = "d-none";
-    //   }
-          x.style.paddingLeft="10px";
-          x.classList.toggle("d-inline-block");
+var x=document.querySelector(".nav-item button");
+var y= document.getElementById("form1");
+x.addEventListener('click',function(){
+          y.style.paddingLeft="10px";
+          y.classList.toggle("d-inline-block");
+});
+
+let masterimg=document.querySelector('.master');
+let imgArr=document.querySelectorAll('#made img');
+let swap;
+for(var i in imgArr){
+if (i>0){
+    imgArr[i].onclick=function(){
+        swap=masterimg.src;
+        masterimg.src=this.src;
+        this.src=swap;
+    }
+}
 }
